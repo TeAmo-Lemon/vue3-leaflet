a4e91d92294da6336ec6097bb75dd3ba155aec88
zhanghuaimin <308765370@qq.com>
2024-04-28 11:01:43 +0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,26 @@ const map = ref(null);  // 使用 ref 来处理响应式数据
 
 // 地图初始化方法
 const initMap = () => {
-  const leafletMap = L.map('map', {
-    center: [24.485666, 118.095498], // 中心位置
-    zoom: 12,  // 缩放级别
+  map.value = L.map('map', {
+    center: [34.210300073739916, 117.13945080254362], // 中心位置
+    zoom: 17,  // 缩放级别
     minZoom: 3,
-    maxZoom: 17,
+    maxZoom: 19,
     attributionControl: false,  // 版权控件
     zoomControl: true  // 缩放控件
   });
 
-  map.value = leafletMap;  // 使用 ref 赋值
+  // map.value = leafletMap;  // 使用 ref 赋值
   L.tileLayer(
     "http://wprd04.is.autonavi.com/appmaptile?lang=zh_cn&size=1&style=7&x={x}&y={y}&z={z}"
-  ).addTo(leafletMap);  // 加载底图
+  ).addTo(map.value);  // 加载底图
 };
 
 // 使用 onMounted 生命周期钩子来初始化地图
 onMounted(() => {
   initMap();
 });
+
 </script>
 
 <style scoped>
